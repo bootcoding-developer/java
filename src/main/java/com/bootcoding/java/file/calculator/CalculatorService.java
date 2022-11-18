4f65db21be91125d3240b2ca6a977fd39ab105e9
bootcoding <bootcoding@gmail.com>
2022-11-18 21:12:35 +0530

    Java File Assignment

---
+++ src/main/java/com/bootcoding/java/file/calculator/CalculatorService.java
@@ -0,0 +1,28 @@
+package com.bootcoding.java.file.calculator;
+
+import java.util.List;
+
+public class CalculatorService {
+
+
+    public void process(List<Number> numbers){
+        numbers.forEach(number -> {
+            add(number);
+            sub(number);
+            mul(number);
+        });
+
+        FileService fs = new FileService();
+        fs.writeToFile(numbers);
+    }
+
+    private void add(Number number){
+        //TODO logic to add
+    }
+    private void sub(Number number){
+        //TODO logic to sub
+    }
+    private void mul(Number number){
+        //TODO logic to mul
+    }
+}
