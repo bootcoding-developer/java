72263266d6ea0e5597e4b478c39cfb1bb48180d5
bootcoding <bootcoding@gmail.com>
2023-01-28 16:23:37 +0530

    test data

---
+++ src/main/java/com/bootcoding/java/basic/HelloWorldBranch.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.bootcoding.java.basic;
+/*
+* Example of Hello World in Java
+* @author bootcoding-developer
+* */
+
+
+public class HelloWorldBranch {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Branch!\nHow are you doing?\n");
+
+
+
+        /*
+         *   -> What is an escape sequence
+         *       two characters of source code that represent a single character.
+         *       (The backslash allows you to escape the string to write special characters.)
+         *
+         *   newline         \n
+         *   tab             \t
+         *   double quote    \"
+         *   backslash       \\
+         *
+         * */
+
+
+
+    }
+}
